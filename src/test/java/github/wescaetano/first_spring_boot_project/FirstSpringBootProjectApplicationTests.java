2d6f0bd47f09e3df56151d0b57046cd2a9b5db5d
wescaetano <weslleycaetanosoares@gmail.com>
2025-03-31 12:29:36 -0300

    Project created

---
+++ src/test/java/github/wescaetano/first_spring_boot_project/FirstSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package github.wescaetano.first_spring_boot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
