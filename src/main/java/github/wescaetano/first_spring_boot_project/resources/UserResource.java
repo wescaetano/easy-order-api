319acc76a6c5039cf2b6745b69c67f31d4508bb8
wescaetano <weslleycaetanosoares@gmail.com>
2025-03-31 13:10:28 -0300

    User entity and resource

---
+++ src/main/java/github/wescaetano/first_spring_boot_project/resources/UserResource.java
@@ -0,0 +1,17 @@
+package github.wescaetano.first_spring_boot_project.resources;
+
+import github.wescaetano.first_spring_boot_project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
