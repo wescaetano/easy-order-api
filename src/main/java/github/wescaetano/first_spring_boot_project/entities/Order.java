e20f07f8821a6574cb39a18a84e298b79ee6ec7b
wescaetano <weslleycaetanosoares@gmail.com>
2025-04-13 10:27:00 -0300

    Payment, one-to-one association

--- src/main/java/github/wescaetano/first_spring_boot_project/entities/Order.java
+++ src/main/java/github/wescaetano/first_spring_boot_project/entities/Order.java
@@ -32,6 +32,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -74,6 +77,14 @@ public class Order implements Serializable {
         if(orderStatus != null){
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems(){
