b32720a582b39318e54c9656fb3d2623552c4b41
wescaetano <weslleycaetanosoares@gmail.com>
2025-04-15 10:22:26 -0300

    User delete

--- src/main/java/github/wescaetano/first_spring_boot_project/resources/UserResource.java
+++ src/main/java/github/wescaetano/first_spring_boot_project/resources/UserResource.java
@@ -32,9 +32,15 @@ public class UserResource {
     }
 
     @PostMapping
-    public ResponseEntity<User> isert(@RequestBody User obj){
+    public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
